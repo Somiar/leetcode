2732e51fb404773aa1268042a186a4cf262e9c9e
Somiar <somiar2010@hotmail.com>
2019-05-15 12:32:56 +0800

    update

--- sudoku-solver.js
+++ sudoku-solver.js
@@ -2,6 +2,7 @@
  * @param {character[][]} board
  * @return {void} Do not return anything, modify board in-place instead.
  */
+ // 版本1 运行5088ms
 var solveSudoku = function(board) {
 	
 	function isValidSudoku(board) {
@@ -15,15 +16,12 @@ var solveSudoku = function(board) {
 			let row = {1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0, '.':0};
 			for(let j = 0; j < 9; j++) {
 				row[board[i][j]] += 1;
-				if(board[i][j] !== '.' && row[board[i][j]] > 1) {
-					return false;
-				}
 				cols[j][board[i][j]] += 1;
-				if(board[i][j] !== '.' && cols[j][board[i][j]] > 1) {
-					return false;
-				}
 				grids[parseInt(i/3)*3+parseInt(j/3)][board[i][j]] += 1;
-				if(board[i][j] !== '.' && grids[parseInt(i/3)*3+parseInt(j/3)][board[i][j]] > 1) {
+				if( board[i][j] !== '.' &&
+					(row[board[i][j]] > 1 ||
+					cols[j][board[i][j]] > 1 || 
+					grids[parseInt(i/3)*3+parseInt(j/3)][board[i][j]] > 1)) {
 					return false;
 				}
 			}
@@ -31,6 +29,8 @@ var solveSudoku = function(board) {
 		return true
 	}
 	// isValidSudoku(board);
+
+	// 从1-9每个数字不断测试验证
 	function dfs(board, x, y) {
 		if(x > 8 || y > 8) {
 			return true;
@@ -66,7 +66,51 @@ var solveSudoku = function(board) {
 	}
 	dfs(board, 0, 0);
 	console.log(board);
+	return board;
 };
+
+
+// 版本2 运行时间88ms
+// 在有效性验证部分，填充一个数字后无需验证整个数独板块的有效性
+// 只需验证该数字所在行、列、3*3板块中的有效性
+var solveSudoku1 = function(board) {
+	function isValidSudoku(row, col, num) {
+		for(let r = 0; r < 9; r++) {
+			if(board[r][col] === num) return false;
+		}
+		for(let c = 0; c < 9; c++) {
+			if(board[row][c] === num) return false;
+		}
+		for(let i = 0; i < 3;  i++) {
+			for(let j = 0; j < 3; j++) {
+				if(board[parseInt(row/3)*3+i][parseInt(col/3)*3+j] === num) return false;
+			}
+		}
+		return true;
+	}
+	function dfs(x, y) {
+		if (x === 9) return true;
+		if (y === 9) return dfs(x + 1, 0);
+		if(board[x][y] === '.') {
+			for(let num = 1; num <= 9; num++) {
+				if(isValidSudoku(x, y, String(num))) {
+					board[x][y] = String(num);
+					if(dfs(x, y+1)) return true;
+					board[x][y] = '.';
+				}
+			}
+		} else {
+			return dfs(x, y+1);
+		}
+		return false;
+	}
+	dfs(0, 0);
+	console.log(board);
+	return board;
+}
+
+
+
 var board = [
   ["5","3",".",".","7",".",".",".","."],
   ["6",".",".","1","9","5",".",".","."],
@@ -78,4 +122,4 @@ var board = [
   [".",".",".","4","1","9",".",".","5"],
   [".",".",".",".","8",".",".","7","9"]
 ]
-solveSudoku(board);
+solveSudoku1(board);
