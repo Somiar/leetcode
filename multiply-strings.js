e70970f5b77f4196fe6f7cdf1e8b6bfffb7efcd9
ychost <ychost@outlook.com>
2019-05-28 22:03:58 +0800

    'update'

---
+++ multiply-strings.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} num1
+ * @param {string} num2
+ * @return {string}
+ */
+ // 43.字符串相乘
+var multiply = function(num1, num2) {
+	let nums = new Array(num1.length+num2.length).fill(0);
+	for(let i = num1.length - 1; i >= 0; i--) {
+		let temp = 0;
+		for(let j = num2.length - 1; j >= 0; j--) {
+			console.log(i + j + 1, num1[i], num2[j])
+			nums[i + j + 1] += (Number(num1[i]) * Number(num2[j]));
+			temp = parseInt(nums[i + j + 1] / 10);
+			nums[i + j + 1] = nums[i + j + 1] % 10;
+			nums[i + j] += temp;
+		}
+	}
+	console.log(nums);
+	let result = nums.join('');
+	result = nums[0] === 0 ? result.slice(1) : result;
+	console.log(result);
+};
+
+multiply('123', '456');
